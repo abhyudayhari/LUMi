76c46f3ea51a7b6e24c377c235cdd08dbd5cdac9
abhyudayhari <abhyudayhari@gmail.com>
2024-10-27 23:28:45 +0530

    basic_api_function

--- ml/apis.py
+++ ml/apis.py
@@ -9,17 +9,15 @@ from dotenv import load_dotenv
 load_dotenv() 
 from tools import *
 from datetime import date
-
+import random
 # Get only today's date
 
 
 #### loading the models##########
 
-main_streamer = TextStreamer(tokenizer, skip_prompt=True)
-translation_streamer=TextStreamer(translational_tokenizer, skip_prompt=True)
 model_id = "meta-llama/Meta-Llama-3.1-8B-Instruct"
 
-login(hf_token)
+# login(hf_token)
 
 bnb_config = transformers.BitsAndBytesConfig(
     load_in_4bit=True,
@@ -40,7 +38,13 @@ asr_model_id = "facebook/mms-1b-l1107"
 asr_processor = AutoProcessor.from_pretrained(asr_model_id)#.to('cuda')
 asr_model = Wav2Vec2ForCTC.from_pretrained(asr_model_id).to('cuda')
 
+#streamers
+
+streamer = TextStreamer(tokenizer, skip_prompt=True)
+translation_streamer=TextStreamer(translational_tokenizer, skip_prompt=True)
+
 ##########defining tools
+
 
 part_1_tools=[
     fetch_user_personal_details,
@@ -107,10 +111,10 @@ def function_call(input_string:str):
 
 
 class bookings():
-    def __init___(self):
+    def __init__(self):
         self.booking_chats={}
 
-    def get_or_create_booking_chats(self,booking_id,content):
+    def get_or_create_booking_chats(self,booking_id,content=None):
         if booking_id not in self.booking_chats:
             booking_details=room_booking_info(booking_id)
             user_id,room_no=booking_details["User_id"],booking_details["Room No"]
@@ -139,20 +143,125 @@ class bookings():
          {"role":"system","content":"Add the special <function> tags before and after the function call or you will be killed immediately"},
           {"role":"system","content":"If the tool is showing error then say, sorry for the inconvinience, right now I am facing some technical difficulties"},
          ]
-        self.booking_chats[booking_id].append(content)
+        if content!=None:
+            self.booking_chats[booking_id].append(content)
         return self.booking_chats[booking_id]
-    # def modify_chats(self,booking_id,content):
-    #     self.booking_chats[booking_id].append(content)
-    #     return self.booking_chats
 
 # class chats_retrieval()
 #     def __init__(self):
 #         self.booking_chats=bookings()
 #     def chat(booking_id,content):
 #         return 
+waiting_messages=["Please hold on for a moment while we complete the operation.!!!","Thank you for your patience; your request is being handled.","Just a moment, we're processing that for you.","Please bear with us; your operation is underway."]
 
-def whatsapp_multi_lang_api(booking_id,content,language):
-    language_codes={"Hindi":{"translation_lang_code":
+booking_chats=bookings()
+def whatsapp_multi_lang_api(booking_id,user_input,language,streaming=False):
+    eng_flag=False
+    language_codes={"Hindi":{"translation_lang_code":"hin_Deva"},"Kannada":{"translation_lang_code":"kan_Knda"}}
+    if language=="English":
+        eng_flag=True
+    else:
+        translation_lang_code=language_codes[language]["translation_lang_code"]
+    if not eng_flag:
+        translational_inputs= translational_tokenizer(user_input, return_tensors="pt",).to("cuda")
+        translated_tokens = translational_model.generate(
+        **translational_inputs, forced_bos_token_id=translational_tokenizer.convert_tokens_to_ids("eng_Latn"),max_length=1024)
+        translated_text=translational_tokenizer.batch_decode(translated_tokens, skip_special_tokens=True)[0]
+        user_chats=booking_chats.get_or_create_booking_chats(booking_id,content={"role":"user","content":translated_text})
+    else:
+        user_chats=booking_chats.get_or_create_booking_chats(booking_id,content={"role":"user","content":user_input})
+
+    inputs=tokenizer.apply_chat_template(user_chats, tools=part_1_tools, add_generation_prompt=True, return_dict=True, return_tensors="pt")
+    inputs = {k: v.to(model.device) for k, v in inputs.items()}
+    # if eng_flag:
+    out = model.generate(**inputs, max_new_tokens=1024) #,
+    output=tokenizer.decode(out[0][len(inputs["input_ids"][0]):],skip_special_tokens=True)
+    # print("THe output is      \n",output)
+    user_chats=booking_chats.get_or_create_booking_chats(booking_id,content={"role":"assistant","content":output})
+    if ("<function>"  in output) or ("{" in output):
+        # tool_start_time=time.time()
+        fn_output=function_call(output)
+        # tool_end_time=time.time()
+        # print("Time taken by tool:",tool_end_time-tool_start_time)
+        # messages.append({"role":"assistant","content":output})
+        tool_output,func_name=fn_output["fn_output"],fn_output["name"]
+        # print(fn_output)
+        # print("The formatted string is : \n",formatted_str,"\n")
+        # print("The tool output is : \n",tool_output)
+        # print(fn_output)
+        if "formatted_output"  in fn_output.keys(): 
+            formatted_str=fn_output["formatted_output"]
+            if formatted_str==None:
+                formatted_str=waiting_messages[random.randint(0,len(waiting_messages)-1)]
+            if not eng_flag:
+                translational_inputs = translational_tokenizer(formatted_str, return_tensors="pt",).to("cuda")
+                if streaming:
+                    translated_tokens = translational_model.generate(
+          **translational_inputs, forced_bos_token_id=translational_tokenizer.convert_tokens_to_ids(translation_lang_code),streamer=translation_streamer, max_length=1024
+          )
+                else:
+                    translated_tokens = translational_model.generate(**translational_inputs, forced_bos_token_id=translational_tokenizer.convert_tokens_to_ids(translation_lang_code),max_length=1024
+          )
+                    
+                    translational_output=translational_tokenizer.batch_decode(translated_tokens, skip_special_tokens=True)[0]
+                    print(translational_output)
+            else:
+                print(formatted_str)
+        else:
+            pass 
+    
+        user_chats=booking_chats.get_or_create_booking_chats(booking_id,content={"role":"assistant","content":f"The output from the tool {func_name} is  {tool_output}"})
+        inputs=tokenizer.apply_chat_template(user_chats, tools=part_1_tools, add_generation_prompt=True, return_dict=True, return_tensors="pt")
+        inputs = {k: v.to(model.device) for k, v in inputs.items()}
+        # if eng_flag:
+        if eng_flag and streaming:
+            out = model.generate(**inputs, streamer=streamer,max_new_tokens=1024) #,
+        else:
+            out=model.generate(**inputs,max_new_tokens=1024) 
+        output=tokenizer.decode(out[0][len(inputs["input_ids"][0]):],skip_special_tokens=True)
+        # print("THe output from model is      \n",output)
+        user_chats=booking_chats.get_or_create_booking_chats(booking_id,content={"role":"assistant","content":output})
+        if not eng_flag:
+            translational_inputs = translational_tokenizer(output, return_tensors="pt",).to("cuda")
+            if streaming:
+                    translated_tokens = translational_model.generate(
+          **translational_inputs, forced_bos_token_id=translational_tokenizer.convert_tokens_to_ids(translation_lang_code),streamer=translation_streamer, max_length=1024
+          )
+            else:
+                    translated_tokens = translational_model.generate(**translational_inputs, forced_bos_token_id=translational_tokenizer.convert_tokens_to_ids(translation_lang_code),max_length=1024
+          )
+                    
+            translational_output=translational_tokenizer.batch_decode(translated_tokens, skip_special_tokens=True)[0]
+            print(translational_output)
+        else:
+            print(output)
+    
+    else:
+        if  eng_flag:
+            print(output)
+        else:
+            translational_inputs = translational_tokenizer(output, return_tensors="pt",).to("cuda")
+            if streaming:
+                    
+                    translated_tokens = translational_model.generate(
+          **translational_inputs, forced_bos_token_id=translational_tokenizer.convert_tokens_to_ids(translation_lang_code),streamer=translation_streamer, max_length=1024
+          )
+            else:
+                    translated_tokens = translational_model.generate(**translational_inputs, forced_bos_token_id=translational_tokenizer.convert_tokens_to_ids(translation_lang_code),max_length=1024
+          )
+            # print("hello")
+            translational_output=translational_tokenizer.batch_decode(translated_tokens, skip_special_tokens=True)[0]
+            print(translational_output)   
+        
+    
+        
+        
+    
+    
+    
+    
+    
+    
 
 
     
