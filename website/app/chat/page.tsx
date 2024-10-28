e3def17398daff96dba238e823f5fb70b3dbad91
Akash <akashsingh2210670@gmail.com>
2024-10-29 03:49:38 +0800

    Refactor API endpoint URL in chat page component

--- website/app/chat/page.tsx
+++ website/app/chat/page.tsx
@@ -45,7 +45,8 @@ const page = () => {
   };
 
   async function run(inputMsg: string) {
-    const apiUrl = `http://34.19.70.196:5000/api/whatsapp_api/`;
+    const apiUrl = `http://34.19.101.149:5000/api/whatsapp_api/`;
+    
     
     const bodyParams = {
       booking_id: bookingID,
